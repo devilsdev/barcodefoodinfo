8ec7d786cb770f56955a7b2bc633cdfeed6ac36c
devilsdev <maik.pleines@googlemail.com>
2018-08-23 21:46:42 +0200

    test

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="productInfo">
     <v-card>
-        <v-card-media
-            v-bind:src="product.product.image_ingredients_url"
-            height="300px"
-        ></v-card-media>
-
         <v-card-title primary-title>
         <div>
             <h3 class="headline mb-0">{{ product.product.product_name }}</h3>
@@ -16,6 +11,17 @@
         <v-card-actions>
             <v-btn flat color="warning" @click="close">Close</v-btn>
         </v-card-actions>
+
+        
+        <v-list>
+          <v-divider><v-divider>
+          <li 
+            v-for="nutriment in product.nutriments"
+            :key="nutriment"
+          ></li>
+        </v-list>
+
+
     </v-card>
   </div>
 </template>
